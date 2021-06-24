35046b5bd72130fa20df93b9d2dcd525dce3766e
tourdiom <mx_santos@hotmail.com>
2021-06-24 11:05:57 -0300

    Update PerguntasRespostas.py

--- PerguntasRespostas.py
+++ PerguntasRespostas.py
@@ -8,28 +8,28 @@ print('Digite apenas a letra que corresponde com a respotas exemplo : a, b, c ou
 
 perguntas = {
     'Pergunta1': {
-        'pergunta' : 'Quanto é 2 + 2?',
-        'respostas': {'a': '1','b': '2','c': '4', 'd':'5',},
+        'pergunta': 'Quanto é 2 + 2?',
+        'respostas': {'a': '1', 'b': '2', 'c': '4', 'd': '5', },
         'resposta_certa': 'c',
     },
     'Pergunta2': {
         'pergunta': 'Quanto é 13 - 6?',
-        'respostas': {'a': '7','b': '8','c': '4', 'd':'5',},
+        'respostas': {'a': '7', 'b': '8', 'c': '4', 'd': '5', },
         'resposta_certa': 'a',
     },
     'Pergunta3': {
         'pergunta': 'Quanto é 5 * 5?',
-        'respostas': {'a': '20','b': '25','c': '40', 'd':'10',},
+        'respostas': {'a': '20', 'b': '25', 'c': '40', 'd': '10', },
         'resposta_certa': 'b',
     },
     'Pergunta4': {
         'pergunta': 'Quanto é 2 * 5?',
-        'respostas': {'a': '7','b': '11','c': '9', 'd':'10',},
+        'respostas': {'a': '7', 'b': '11', 'c': '9', 'd': '10', },
         'resposta_certa': 'd',
     },
     'Pergunta5': {
         'pergunta': 'Quanto é 3 * 5?',
-        'respostas': {'a': '8','b': '11','c': '15', 'd':'10',},
+        'respostas': {'a': '8', 'b': '11', 'c': '15', 'd': '10', },
         'resposta_certa': 'c',
     },
 }
@@ -43,7 +43,7 @@ for pk, pv in perguntas.items():
     for rk, rv in pv['respostas'].items():
         print(f'[{rk}]: {rv}')
 
-    respostas_usuario= input('Sua resposta é: ')
+    respostas_usuario = input('Sua resposta é: ')
 
     if respostas_usuario == pv['resposta_certa']:
         print('Acertou miseravil')
